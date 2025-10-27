9b9485bd2548c855f253852f0f6682920d5396ba
docdavidgrundy <david@grundymdconsulting.com>
2025-10-27 14:57:25 -0700

    Create ping.js

---
+++ api/actions/ping.js
@@ -0,0 +1,8 @@
+// api/actions/ping.js
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", "POST");
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+  return res.status(200).json({ ok: true, result: "pong" });
+}
